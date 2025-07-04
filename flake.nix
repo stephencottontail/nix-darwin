eebb9605d7303caa3bbcc6345542efd9d20d4a80
Stephen Dickinson <stephencottontail@me.com>
2025-07-03 19:24:45 -0600

    Set up ZSH and formatting tools

--- flake.nix
+++ flake.nix
@@ -5,45 +5,108 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # Let Determinate Systems manage Nix
-      nix.enable = false;
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      home-manager,
+    }:
+    let
+      configuration =
+        { pkgs, ... }:
+        {
+          # Let Determinate Systems manage Nix
+          nix.enable = false;
 
-      # TouchID for sudo
-      security.pam.services.sudo_local.touchIdAuth = true;
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages = [ pkgs.plan9port ];
+          # TouchID for sudo
+          security.pam.services.sudo_local.touchIdAuth = true;
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [
+            pkgs.plan9port
+            pkgs.nixfmt-rfc-style
+          ];
 
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+          # Enable alternative shell support in nix-darwin.
+          # programs.fish.enable = true;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Necessary now that `darwin-rebuild` runs as root
-      system.primaryUser = "stephen";
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 6;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+          # Let's see how far we can go without home-manager
+          users.users.stephen = {
+            name = "stephen";
+            home = "/Users/stephen";
+          };
+
+          # ZSH
+          programs.zsh = {
+            enable = true;
+            promptInit = ''
+              export PROMPT="blueberry> "
+            '';
+          };
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+        };
+      homeConfiguration =
+        { pkgs, ... }:
+        {
+          # This is apparently required for internal compatibility
+          # Don't mess with it?
+          home.stateVersion = "23.05";
+
+          programs.home-manager.enable = true;
+
+          # ZSH
+          home.file.".zshrc".text = ''
+            # Empty file to suppress new user menu
+          '';
+
+          # Scripts
+          home.file."bin/update" = {
+            executable = true;
+            text = ''
+              #!/usr/bin/env sh
+
+              sudo darwin-rebuild switch --flake /Users/stephen/Documents/Projects/nix-darwin/flake.nix#default
+            '';
+          };
+
+          home.file."bin/foo" = {
+            executable = true;
+            text = "${pkgs.plan9port}";
+          };
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#default
+      darwinConfigurations."default" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.verbose = true;
+            home-manager.users.stephen = homeConfiguration;
+          }
+        ];
+      };
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#default
-    darwinConfigurations."default" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-  };
 }
