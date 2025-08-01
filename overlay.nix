b9c244f5d3d5563d51a1eb28f72b0de72e390280
Stephen Dickinson <stephencottontail@me.com>
2025-07-31 21:49:19 -0600

    Rework application so Acme can accept dropped files

--- overlay.nix
+++ overlay.nix
@@ -3,83 +3,60 @@ final: prev: {
     postInstall =
       (previousAttrs.postInstall or "")
       + ''
-        			PLAN9=$out/plan9/bin
+        PLAN9=$out/plan9/bin
 
-                		mkdir -p $out/Applications/{Acme,Sam}.app/Contents/{MacOS,Resources}
-                		echo -e "#!/usr/bin/env sh\n" \
-                			"\n" \
-                			"$PLAN9/9 acme \"\$*\" &" > $out/Applications/Acme.app/Contents/MacOS/Acme
-                		echo -e "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" \
-                			"<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n" \
-                			"<plist version=\"1.0\">\n" \
-                			"<dict>\n" \
-                			"\t<key>CFBundleDevelopmentRegion</key>\n" \
-                			"\t<string>English</string>\n" \
-                			"\t<key>CFBundleExecutable</key>\n" \
-                			"\t<string>acme</string>\n" \
-                			"\t<key>CFBundleGetInfoString</key>\n" \
-                			"\t<string>0.1-1</string>\n" \
-                			"\t<key>CFBundleIconName</key>\n" \
-                			"\t<string>spaceglenda</string>\n" \
-                			"\t<key>CFBundleIconFile</key>\n" \
-                			"\t<string>spaceglenda.icns</string>\n" \
-                			"\t<key>CFBundleIdentifier</key>\n" \
-                			"\t<string>com.swtch.acme</string>\n" \
-                			"\t<key>CFBundleInfoDictionaryVersion</key>\n" \
-                			"\t<string>6.0</string>\n" \
-                			"\t<key>CFBundleName</key>\n" \
-                			"\t<string>Acme</string>\n" \
-                			"\t<key>CFBundlePackageType</key>\n" \
-                			"\t<string>APPL</string>\n" \
-                			"\t<key>CFBundleShortVersionString</key>\n" \
-                			"\t<string>0.1</string>\n" \
-                			"\t<key>CFBundleSignature</key>\n" \
-                			"\t<string>Acme1</string>\n" \
-                			"\t<key>CFBundleVersion</key>\n" \
-                			"\t<string>1</string>\n" \
-                			"</dict>\n" \
-                			"</plist>" > $out/Applications/Acme.app/Contents/Info.plist
+        mkdir -p $out/Applications/Sam.app/Contents/{MacOS,Resources}
+        echo -e "on run\n" \
+          "\tdo shell script \"$PLAN9/9 acme /Users/stephen &\"\n" \
+          "end run\n" \
+          "on open droppedItems\n" \
+          "\tset paths to \"\"\n" \
+          "\trepeat with a from 1 to count of droppedItems\n" \
+          "\t\tset currentItem to item a of droppedItems\n" \
+          "\t\tset paths to paths & \" \" & POSIX path of currentItem\n" \
+          "\tend repeat\n" \
+          "\tdo shell script \"$PLAN9/9 acme \" & paths & \" &\"\n" \
+          "end open" > $out/Acme.scpt
+        /usr/bin/osacompile -o $out/Applications/Acme.app $out/Acme.scpt
 
-                		echo -e "#!/usr/bin/env sh\n" \
-                			"\n" \
-           			"$PLAN9/9 sam \"\$*\" &" > $out/Applications/Sam.app/Contents/MacOS/Sam
-                		echo -e "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" \
-                			"<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n" \
-                			"<plist version=\"1.0\">\n" \
-                			"<dict>\n" \
-                			"\t<key>CFBundleDevelopmentRegion</key>\n" \
-                			"\t<string>English</string>\n" \
-                			"\t<key>CFBundleExecutable</key>\n" \
-                			"\t<string>sam</string>\n" \
-                			"\t<key>CFBundleGetInfoString</key>\n" \
-                			"\t<string>0.1-1</string>\n" \
-                			"\t<key>CFBundleIconName</key>\n" \
-                			"\t<string>spaceglenda</string>\n" \
-                			"\t<key>CFBundleIconFile</key>\n" \
-                			"\t<string>spaceglenda.icns</string>\n" \
-                			"\t<key>CFBundleIdentifier</key>\n" \
-                			"\t<string>com.swtch.sam</string>\n" \
-                			"\t<key>CFBundleInfoDictionaryVersion</key>\n" \
-                			"\t<string>6.0</string>\n" \
-                			"\t<key>CFBundleName</key>\n" \
-                			"\t<string>Sam</string>\n" \
-                			"\t<key>CFBundlePackageType</key>\n" \
-                			"\t<string>APPL</string>\n" \
-                			"\t<key>CFBundleShortVersionString</key>\n" \
-                			"\t<string>0.1</string>\n" \
-                			"\t<key>CFBundleSignature</key>\n" \
-                			"\t<string>Sam1</string>\n" \
-                			"\t<key>CFBundleVersion</key>\n" \
-                			"\t<string>1</string>\n" \
-                			"</dict>\n" \
-                			"</plist>" > $out/Applications/Sam.app/Contents/Info.plist
+        echo -e "#!/usr/bin/env sh\n" \
+          "\n" \
+          "$PLAN9/9 sam &" >> $out/Applications/Sam.app/Contents/MacOS/Sam
+        echo -e "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" \
+          "<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n" \
+          "<plist version=\"1.0\">\n" \
+          "<dict>\n" \
+          "\t<key>CFBundleDevelopmentRegion</key>\n" \
+          "\t<string>English</string>\n" \
+          "\t<key>CFBundleExecutable</key>\n" \
+          "\t<string>Sam</string>\n" \
+          "\t<key>CFBundleGetInfoString</key>\n" \
+          "\t<string>0.1-1</string>\n" \
+          "\t<key>CFBundleIconName</key>\n" \
+          "\t<string>spaceglenda</string>\n" \
+          "\t<key>CFBundleIconFile</key>\n" \
+          "\t<string>spaceglenda.icns</string>\n" \
+          "\t<key>CFBundleIdentifier</key>\n" \
+          "\t<string>com.swtch.sam</string>\n" \
+          "\t<key>CFBundleInfoDictionaryVersion</key>\n" \
+          "\t<string>6.0</string>\n" \
+          "\t<key>CFBundleName</key>\n" \
+          "\t<string>Sam</string>\n" \
+          "\t<key>CFBundlePackageType</key>\n" \
+          "\t<string>APPL</string>\n" \
+          "\t<key>CFBundleShortVersionString</key>\n" \
+          "\t<string>0.1</string>\n" \
+          "\t<key>CFBundleSignature</key>\n" \
+          "\t<string>Sam1</string>\n" \
+          "\t<key>CFBundleVersion</key>\n" \
+          "\t<string>1</string>\n" \
+          "</dict>\n" \
+          "</plist>" > $out/Applications/Sam.app/Contents/Info.plist
 
-                		chmod a+x $out/Applications/Acme.app/Contents/MacOS/acme
-                		chmod a+x $out/Applications/Sam.app/Contents/MacOS/sam
-
-                		cp $out/plan9/mac/spaceglenda.icns $out/Applications/Acme.app/Contents/Resources
-                		cp $out/plan9/mac/spaceglenda.icns $out/Applications/Sam.app/Contents/Resources
-                		cp -r $out/plan9/mac/9term.app $out/Applications
+        chmod a+x $out/Applications/Sam.app/Contents/MacOS/Sam
+        cp $out/plan9/mac/spaceglenda.icns $out/Applications/Acme.app/Contents/Resources
+        cp $out/plan9/mac/spaceglenda.icns $out/Applications/Sam.app/Contents/Resources
+        cp -r $out/plan9/mac/9term.app $out/Applications
       '';
   });
 }
